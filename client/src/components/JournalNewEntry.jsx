868d87a954b0b9c170eb30ed4886abf1edefc082
sophiarosely <pheeuh@gmail.com>
2023-03-23 17:38:24 -0500

    adding new journal components/gitignore file

---
+++ client/src/components/JournalNewEntry.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function JournalNewEntry () {
+  return (
+    <div></div>
+  );
+}
+
+export default JournalNewEntry; 
